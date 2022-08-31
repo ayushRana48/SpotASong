e912823a0da0b6f7de5deb67586d180ac3be8d55
Ayush Rana <4ayushrana@gmail.com>
2022-08-31 16:45:02 -0700

    I Got playlist export to work!

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -2,13 +2,15 @@ import React, { useEffect } from "react";
 import PlaylistSongs from "./PlaylistSongs";
 import axios from "axios";
 import finalSongList from "../SongData/finalSongList";
-
+import $ from 'jquery'; 
 
 export default function Playlist(props){
     //name of the playlist
     const[playlistName,setPlaylistName]=React.useState("")
     //songs to be exported, from toPlaylist variable in App
     const[songs,setSongs]=React.useState(props.inPlaylist)
+    const[playlistId,setPlayListId]=React.useState()
+
 
     //update the songs every time a song is added or removed from toPlaylist. props.inPlaylist same as toPlaylist in App
     useEffect(()=>{
@@ -55,23 +57,72 @@ export default function Playlist(props){
         const myId= await getMe()
         console.log(myId)
         const token=props.token
+        let name;
+
+        if(playlistName){
+            name=playlistName
+        }
+        else{
+            name="Flub's list"
+        }
 
         const urlPlay="https://api.spotify.com/v1/users/"+myId+"/playlists"
+        let id2;
         //reset playlist when songs are exported bc songs now in spotify acct so should not be in web APP
-        props.changePlaylist([])
-
-        await axios.post(urlPlay,{
-            headers:{
-                Authorization:`Bearer ${props.token}`
+        $.ajax({
+            url: urlPlay,
+            method: "POST",
+            data: JSON.stringify({
+              name:name
+            }),
+            headers: {
+              'Authorization': 'Bearer ' + props.token,
+              'Content-Type': 'application/json'
             },
-            body:
-               {"name": "New Playliddddst",
-                "description": "New playlist description",
-                public: false}
-              
-        })
-        
+            success: function(response) {
+              console.log(response);
+              setPlayListId(response.id);
+              id2=response.id     
+              populatePlaylist(id2)         
+            }
+          })
      }
+
+     async function populatePlaylist(id){
+        const songList=songs;
+        props.changePlaylist([])
+        console.log("populate playlist")
+        console.log(id + "playlist")
+        try{
+        for(let i=0;i<songList.length;i++){
+            
+            const songId=songList[i].id
+            const x="spotify:track:"+songId
+            console.log(x)
+
+            console.log(songId)
+            const playlistUrl="https://api.spotify.com/v1/playlists/"+id+"/tracks"
+            $.ajax({
+                url: playlistUrl,
+                method: "POST",
+                data:JSON.stringify({
+                    uris:[x]
+                  }),
+                headers: {
+                  'Authorization': 'Bearer ' + props.token,
+                  'Content-Type': 'application/json'
+                },
+                success: function(response) {
+                  console.log(response);
+                }
+              });
+        }}
+        catch(err){
+            console.log(err)
+        }
+     }
+
+
 
 
      //utility method used to change song Library
@@ -105,7 +156,7 @@ export default function Playlist(props){
 
                 </div>
            </div>
-           <button onClick={combine} className="searchSong">Generate Playlist</button>
+           <button onClick={makePlaylist} className="searchSong">Generate Playlist</button>
         </div>
         
     )
